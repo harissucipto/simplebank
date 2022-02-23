2beb5c3ff9f2710a47cdf04c9398db1d3650a804
Haris Sucipto <harisssucipto@gmail.com>
2022-02-23 09:16:43 +0800

    add mock for testing http api

--- Makefile
+++ Makefile
@@ -21,5 +21,8 @@ test:
 
 server:
 	go run main.go
+
+mock:
+	mockgen -package mockdb -destination db/mock/store.go github.com/harissucipto/simplebank/db/sqlc Store
 	
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server mock
--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Server servers HTTP requests for our banking services
 type Server struct {
-	store *db.Store 
+	store db.Store 
 	router *gin.Engine
 }
 
 // NewServer creates a new HTTP Server and setup routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
